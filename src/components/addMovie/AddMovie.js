d3189c872f6e109ec12f6c39f763343891e72443
ebedev31 <ebedev31@gmail.com>
2021-02-16 23:28:31 +0100

    add functionality to AddMovie component

--- src/components/addMovie/AddMovie.js
+++ src/components/addMovie/AddMovie.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles/AddMovieStyle.module.scss'
 const AddMovie = () => {
 
-    function addMovie() {
+    const [movieInfo, setMovieInfo] = useState({
+        title: '',
+        genre: '',
+        year: ''
+    })
+
+    function addMovie(e) {
+        e.preventDefault()
+        setMovieInfo({
+            title: '',
+            genre: '',
+            year: ''
+        })
         console.log('movie to be added')
+    }
+
+    function handleOnChange(e) {
+        let input = e.target.name
+        let value = e.target.value
+        setMovieInfo({
+            ...movieInfo,
+            [input]: value
+        })
     }
     return (
         <div className={styles.addMovie}>
@@ -11,15 +32,15 @@ const AddMovie = () => {
             <form onSubmit={addMovie}>
                 <div className={styles.field}>
                     <label>Title:</label>
-                    <input type='text' />
+                    <input type='text' name='title' value={movieInfo.title} onChange={handleOnChange} />
                 </div>
                 <div className={styles.field}>
                     <label>Genre:</label>
-                    <input type='text' />
+                    <input type='text' name='genre' value={movieInfo.genre} onChange={handleOnChange} />
                 </div>
                 <div className={styles.field}>
                     <label>Year:</label>
-                    <input type='text' />
+                    <input type='text' name='year' value={movieInfo.year} onChange={handleOnChange} />
                 </div>
                 <input
                     type='submit'
