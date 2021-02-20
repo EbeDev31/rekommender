1a14745f9d47faf91106c7e2be9df801bf0f4de5
ebedev31 <ebedev31@gmail.com>
2021-02-20 13:50:50 +0100

    clean up

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,5 @@
 const graphql = require('graphql')
 const Movie = require('../models/movie')
-const Book = require('../models/book')
-const Author = require('../models/author')
 const _ = require('lodash');
 
 const {
@@ -14,36 +12,6 @@ const {
     GraphQLNonNull
 } = graphql;
 
-const BookType = new GraphQLObjectType({
-    name: 'Book',
-    fields: () => ({
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
-        author: {
-            type: AuthorType,
-            resolve(parent, args) {
-                return Author.findById(parent.authorId);
-            }
-        }
-    })
-});
-
-const AuthorType = new GraphQLObjectType({
-    name: 'Author',
-    fields: () => ({
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt },
-        books: {
-            type: new GraphQLList(BookType),
-            resolve(parent, args) {
-                return Book.find({ authorId: parent.id });
-            }
-        }
-    })
-});
-
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
     fields: () => ({
@@ -53,7 +21,6 @@ const MovieType = new GraphQLObjectType({
         year: { type: GraphQLString },
     })
 });
-
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
